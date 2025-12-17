c45a2d31654136dd64543cac841f13b33a85ece0
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2025-12-17 11:17:20 +0700

    feat: add cors

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,5 @@
 from server import create_app
+from flask_cors import CORS
 
 application = create_app()
+CORS(application, resources={r"/*": {"origins": ["https://frontend-web-pelayanan-publik-sktm.vercel.app"]}}, supports_credentials=True)
